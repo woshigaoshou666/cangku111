9aec081a84942b813bcff88cdf08267392acdb68
woshigaoshou666 <3146693074@qq.com>
2026-03-06 09:42:10 +0800

    feat: 增加键盘输入功能

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-实现了c语言hello world程序# cangku111
+增加了键盘输入功能，程序会回显用户输入的内容。实现了c语言hello world程序# cangku111
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input[100];
+    printf("请输入内容: ");
+    // 从键盘读取一行输入
+    fgets(input, sizeof(input), stdin);
+    printf("你输入的是: %s", input);
     return 0;
 }
